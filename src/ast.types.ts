7470d35961990cccab305f83b42772f61f7fb698
victor <victor.fouquet@gmail.com>
2024-10-17 23:23:08 +0200

    Add ast related types

---
+++ src/ast.types.ts
@@ -0,0 +1,26 @@
+import {
+    AggregateOperators,
+    CollectionLeaves,
+    InferCollectionOperator,
+    InferCollectionValueType,
+    InferOperator,
+    InferValueType,
+    Leaves,
+} from "./query.types"
+
+export type ValueNode<Leaf, Entity> = {
+    op: InferOperator<Leaf, Entity> | InferCollectionOperator<Leaf, Entity>,
+    value: InferValueType<Leaf, Entity> | InferCollectionValueType<Leaf, Entity>
+}
+
+export type FieldNode<Leaf extends Leaves<Entity>, Entity> = {
+    [K in Leaf]: ValueNode<K, Entity>
+}
+
+export type AggregateNode<Agg extends AggregateOperators, Entity> = {
+    [K in Agg]: FieldNode<CollectionLeaves<Entity>, Entity>
+}
+
+export type WhereNode<Entity> = {
+    where: Partial<AggregateNode<AggregateOperators, Entity> & FieldNode<Leaves<Entity>, Entity>>
+}
