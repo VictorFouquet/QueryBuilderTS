c5367ea1d2f3a486fd18fd895b1bb6b56c9d4538
victor <victor.fouquet@gmail.com>
2024-10-20 18:05:18 +0200

    Add where range unit tests

--- test/where-clause.test.ts
+++ test/where-clause.test.ts
@@ -2,7 +2,7 @@ import { PrimitiveLeaves } from "../src/ast.types";
 import { Query } from "../src/query-builder.abstraction"
 import { CollectionLeaves } from "../src/query.types";
 
-test('Query should support filtering on numerical values with different operators', () => {
+test('Query should support filtering on numerical values with different numerical operators', () => {
     interface Queryable { id: number };
     class Q extends Query<Queryable, PrimitiveLeaves<Queryable>, CollectionLeaves<Queryable>> {};
 
@@ -25,7 +25,26 @@ test('Query should support filtering on numerical values with different operator
     .toStrictEqual({ where: { id: { op: "neq", value: 1 } }});
 })
 
-test('Query should support filtering on date values with different operators', () => {
+test('Query should support filtering on numerical values with different range operators', () => {
+    interface Queryable { id: number };
+    class Q extends Query<Queryable, PrimitiveLeaves<Queryable>, CollectionLeaves<Queryable>> {};
+
+    expect(new Q().where('id', 'eq', 1).whereClause)
+
+    expect(new Q().where('id', 'gt_lt', [0, 2]).whereClause)
+    .toStrictEqual({ where: { id: { op: "gt_lt", value: [0, 2] } }});
+
+    expect(new Q().where('id', 'gt_lte', [0, 1]).whereClause)
+    .toStrictEqual({ where: { id: { op: "gt_lte", value: [0, 1] } }});
+
+    expect(new Q().where('id', 'gte_lt', [1, 2]).whereClause)
+    .toStrictEqual({ where: { id: { op: "gte_lt", value: [1, 2] } }});
+
+    expect(new Q().where('id', 'gte_lte', [1, 1]).whereClause)
+    .toStrictEqual({ where: { id: { op: "gte_lte", value: [1, 1] } }});
+})
+
+test('Query should support filtering on date values with different numerical operators', () => {
     interface Queryable { due: Date };
     class Q extends Query<Queryable, PrimitiveLeaves<Queryable>, CollectionLeaves<Queryable>> {};
 
@@ -48,7 +67,24 @@ test('Query should support filtering on date values with different operators', (
     .toStrictEqual({ where: { due: { op: "neq", value: new Date('01-01-2024') } }});
 })
 
-test('Query should support filtering on literal values with different operators', () => {
+test('Query should support filtering on date values with different range operators', () => {
+    interface Queryable { due: Date };
+    class Q extends Query<Queryable, PrimitiveLeaves<Queryable>, CollectionLeaves<Queryable>> {};
+
+    expect(new Q().where('due', 'gt_lt', [new Date('01-01-2024'), new Date('01-31-2024')]).whereClause)
+    .toStrictEqual({ where: { due: { op: "gt_lt", value: [new Date('01-01-2024'), new Date('01-31-2024')] } }});
+
+    expect(new Q().where('due', 'gt_lte', [new Date('01-01-2024'), new Date('01-31-2024')]).whereClause)
+    .toStrictEqual({ where: { due: { op: "gt_lte", value: [new Date('01-01-2024'), new Date('01-31-2024')] } }});
+
+    expect(new Q().where('due', 'gte_lt', [new Date('01-01-2024'), new Date('01-31-2024')]).whereClause)
+    .toStrictEqual({ where: { due: { op: "gte_lt", value: [new Date('01-01-2024'), new Date('01-31-2024')] } }});
+
+    expect(new Q().where('due', 'gte_lte', [new Date('01-01-2024'), new Date('01-31-2024')]).whereClause)
+    .toStrictEqual({ where: { due: { op: "gte_lte", value: [new Date('01-01-2024'), new Date('01-31-2024')] } }});
+})
+
+test('Query should support filtering on literal values with different literal operators', () => {
     interface Queryable { name: string };
     class Q extends Query<Queryable, PrimitiveLeaves<Queryable>, CollectionLeaves<Queryable>> {};
 
@@ -65,7 +101,7 @@ test('Query should support filtering on literal values with different operators'
     .toStrictEqual({ where: { name: { op: 'endswith', value: 'abc' } }});
 })
 
-test('Query should support filtering on boolean values with different operators', () => {
+test('Query should support filtering on boolean values with different boolean operators', () => {
     interface Queryable { closed: boolean };
     class Q extends Query<Queryable, PrimitiveLeaves<Queryable>, CollectionLeaves<Queryable>> {};
 
